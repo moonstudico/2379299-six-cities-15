fb17310ca3df105a35b7f86e7196fffad453cb40
Moon Studico <moonstudico@gmail.com>
2024-02-26 14:44:06 +0100

    add data

---
+++ src/mocks/offers.ts
@@ -0,0 +1,99 @@
+import {Offer} from '../types/offers';
+
+
+const offers: Offer[] = [
+  {
+    id: '9772b366-ef03-4bc4-a728-2e885fc44eac',
+    title: 'Tile House',
+    type: 'apartment',
+    price: 321,
+    previewImage: 'https://15.design.htmlacademy.pro/static/hotel/9.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 48.868610000000004,
+      longitude: 2.342499,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 1.5
+  },
+  {
+    id: '2f47ca94-4d24-4425-9ae6-43d7333df736',
+    title: 'House in countryside',
+    type: 'apartment',
+    price: 242,
+    previewImage: 'https://15.design.htmlacademy.pro/static/hotel/3.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 48.858610000000006,
+      longitude: 2.330499,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 2.7
+  },
+  {
+    id: '258e916e-b073-49d5-8806-e4e2fb3617a3',
+    title: 'Tile House',
+    type: 'apartment',
+    price: 101,
+    previewImage: 'https://15.design.htmlacademy.pro/static/hotel/6.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 48.834610000000005,
+      longitude: 2.335499,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: false,
+    rating: 2
+  },
+  {
+    id: 'f431d956-6b7f-4aa4-839e-d57743638b28',
+    title: 'Tile House',
+    type: 'room',
+    price: 216,
+    previewImage: 'https://15.design.htmlacademy.pro/static/hotel/19.jpg',
+    city: {
+      name: 'Paris',
+      location: {
+        latitude: 48.85661,
+        longitude: 2.351499,
+        zoom: 13
+      }
+    },
+    location: {
+      latitude: 48.85761,
+      longitude: 2.358499,
+      zoom: 16
+    },
+    isFavorite: false,
+    isPremium: true,
+    rating: 5
+  }
+];
+
+export {offers};
--- src/pages/main-page/main-page.tsx
+++ src/pages/main-page/main-page.tsx
@@ -1,4 +1,5 @@
 import OfferCard from '../../component/offer-card';
+import ComponentLiLocations from './component-li-locations';
 
 type MainPageProps = {
   placeCount: number;
@@ -11,36 +12,12 @@ function MainPage({placeCount}: MainPageProps): JSX.Element {
       <div className="tabs">
         <section className="locations container">
           <ul className="locations__list tabs__list">
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Paris</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Cologne</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Brussels</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item tabs__item--active">
-                <span>Amsterdam</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Hamburg</span>
-              </a>
-            </li>
-            <li className="locations__item">
-              <a className="locations__item-link tabs__item" href="#">
-                <span>Dusseldorf</span>
-              </a>
-            </li>
+            <ComponentLiLocations />
+            <ComponentLiLocations />
+            <ComponentLiLocations />
+            <ComponentLiLocations />
+            <ComponentLiLocations />
+            <ComponentLiLocations />
           </ul>
         </section>
       </div>
