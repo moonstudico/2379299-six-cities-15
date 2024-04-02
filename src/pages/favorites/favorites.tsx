edc5963139754fe8fa0f7c478a72c7fb2281dd21
Moon Studico <moonstudico@gmail.com>
2024-04-02 17:52:38 +0200

    fix page favorite

--- src/pages/favorites/favorites.tsx
+++ src/pages/favorites/favorites.tsx
@@ -12,15 +12,24 @@ function Favorites(): JSX.Element {
   const uniqueCities = [...new Set(favorites.map((favorite) => favorite.city.name))];
 
   return (
-    <main className={`page__main page__main--favorites ${favorites.length > 0 ? 'page__main--favorites-empty' : ''}`}>
+    <main className={`page__main page__main--favorites ${favorites.length > 0 ? '' : 'page__main--favorites-empty'}`}>
       <div className="page__favorites-container container">
-        <section className="favorites">
-          <h1 className="favorites__title">Saved listing</h1>
-          <ul className="favorites__list">{
-            uniqueCities.map((city) => (<FavoritesCity favorites={favorites} city={city as City} key={city} />))
-          }
-          </ul>
-        </section>
+        {favorites.length > 0 ? (
+          <section className="favorites">
+            <h1 className="favorites__title">Saved listing</h1>
+            <ul className="favorites__list">{
+              uniqueCities.map((city) => (<FavoritesCity favorites={favorites} city={city as City} key={city} />))
+            }
+            </ul>
+          </section>) : (
+          <section className="favorites favorites--empty">
+            <h1 className="visually-hidden">Favorites (empty)</h1>
+            <div className="favorites__status-wrapper">
+              <b className="favorites__status">Nothing yet saved.</b>
+              <p className="favorites__status-description">Save properties to narrow down search or plan your future trips.</p>
+            </div>
+          </section>
+        )}
       </div>
     </main>
   );
