89dd202d535cf1a5c909e39983e2bde8f97c888d
Moon Studico <moonstudico@gmail.com>
2024-04-09 12:30:32 +0200

    fix

--- src/store/index.ts
+++ src/store/index.ts
@@ -3,7 +3,9 @@ import { createAPI } from '../services/api';
 import { offersReduser } from './offers-reduser/offers -reduser';
 import { cityReduser } from './city-reduser/city-reduser';
 import { loadingReduser } from './loading-reduser/loading-reduser';
-import { userReduser } from './user-reduser /user-reduser';
+import { userReduser } from './user-reduser/user-reduser';
+
+
 
 const rootReducer = combineReducers({
   offers: offersReduser.reducer,
