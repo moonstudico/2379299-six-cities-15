df97d90dce0abde209449ac26fcfbbebd42fbde5
Moon Studico <moonstudico@gmail.com>
2024-04-09 12:25:37 +0200

    fix file path

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,9 @@
 import { combineReducers, configureStore } from '@reduxjs/toolkit';
 import { createAPI } from '../services/api';
 import { offersReduser } from './offers-reduser/offers -reduser';
-import { userReduser } from './user-reduser /user-reduser.ts ';
 import { cityReduser } from './city-reduser/city-reduser';
 import { loadingReduser } from './loading-reduser/loading-reduser';
+import { userReduser } from './user-reduser /user-reduser';
 
 const rootReducer = combineReducers({
   offers: offersReduser.reducer,
